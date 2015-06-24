347bf6e5ddba1821dfb5080d66c3a1c8fc95a8e4
Carlos Rodriguez <carlos.d.rodriguez@pwc.com>
2015-06-23 23:25:12 -0400

    Modified step to match scneario

--- test/functional/steps/then.js
+++ test/functional/steps/then.js
@@ -6,7 +6,7 @@ var expect = chai.expect;
 module.exports = function() {
   this.World = require('../support/world');
 
-  this.Then(/^I get (\d+) results for "([^"]*)"$/,
+  this.Then(/^I should get (\d+) results for "([^"]*)"$/,
     function (total, term, done) {
     // Write code here that turns the phrase above into concrete actions
     done.pending();
