dbfcfdc15a3e979c89bcfbb1e2475f061ca4661d
Carlos Rodriguez <carlos.d.rodriguez@pwc.com>
2015-06-29 22:25:22 -0400

    Added mock to search test

--- test/search.js
+++ test/search.js
@@ -2,7 +2,13 @@
 
 'use strict';
 
-var mockData = require('./support');
+var nock = require('nock');
+
+var mockData = nock('https://api.fda.gov')
+  .get('/drug/label.json?search=openfda.brand_name:hero+openfda.generic_name:hero&limit=10&api_key=fWo3VYTbToPaLcFem1T9ZJqqNHNmetmU3peGa1BF')
+  .replyWithFile(200, __dirname + '/mocks/search.json', {
+    'Content-Type': 'application/json'
+  });
 
 
 var kraken = require('kraken-js'),
