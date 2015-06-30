43758612bc7af500b505533bab852a6a32b09d9b
ivanfrank <ivan.f.aguirre@us.pwc.com>
2015-06-30 17:02:32 -0400

    refactoring info and label search controllers

--- models/openFDA/drugInfoResponse.js
+++ models/openFDA/drugInfoResponse.js
@@ -1,23 +1,29 @@
 'use strict';
 
 var DrugLabelModel = function (searchResponse) {
-    var responseObject = JSON.parse(searchResponse);
-    var hitCount = (responseObject.meta.results.limit < responseObject.meta.results.total) ? 
-        responseObject.meta.results.limit : responseObject.meta.results.total;   //@TODO: handle 100+
+    var responseObject;
+    var hitCount = 0;
     var brandNames = []; 
     var genericNames = [];
     var drugInfo = {};
 
-    //take only the first drug matched
-    if (responseObject.results[0].openfda !== undefined) {
-        // has openFDA data
-        drugInfo.brandName = responseObject.results[0].openfda.brand_name.toString();
-        drugInfo.genericName = responseObject.results[0].openfda.generic_name.toString();
-        drugInfo.indications = (responseObject.results[0].indications_and_usage) ? 
-            responseObject.results[0].indications_and_usage.toString().trim() : '';
-        drugInfo.contraindications = (responseObject.results[0].contraindications) ? 
-            responseObject.results[0].contraindications.toString().trim() : '';
-    } 
+    if (searchResponse !== null) {
+        responseObject = JSON.parse(searchResponse);
+        hitCount = (responseObject.meta.results.limit < responseObject.meta.results.total) ? 
+            responseObject.meta.results.limit : responseObject.meta.results.total;
+            //no need to handle 100+ result queries so far
+
+        //take only the first drug matched
+        if (responseObject.results[0].openfda !== undefined) {
+            // has openFDA data
+            drugInfo.brandName = responseObject.results[0].openfda.brand_name.toString();
+            drugInfo.genericName = responseObject.results[0].openfda.generic_name.toString();
+            drugInfo.indications = (responseObject.results[0].indications_and_usage) ? 
+                responseObject.results[0].indications_and_usage.toString() : '';
+            drugInfo.contraindications = (responseObject.results[0].contraindications) ? 
+                responseObject.results[0].contraindications.toString() : '';
+        } 
+    }
 
     return {
         drugInfo: drugInfo
